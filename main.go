99508ef8c8c22a103eedbe2876231bc91de1ae6a
mohamed.shokry <mohamed.shokry@flairstech.com>
2024-10-10 01:15:27 +0300

    quick sort

--- main.go
+++ main.go
@@ -3,20 +3,22 @@ package main
 func main() {
 	A := []int{5, 2, 4, 6, 1, 3}
 	n := len(A)
-	insertionSort(A, n)
+	quickSort(A, n)
 	for i := 0; i < n; i++ {
 		println(A[i])
 	}
 }
 
-func insertionSort(A []int, n int) {
-	for i := 1; i < n; i++ {
-		key := A[i]
-		j := i - 1
-		for j >= 0 && A[j] > key {
-			A[j+1] = A[j]
-			j--
+func quickSort(A []int, n int) {
+	for i := 0; i < n; i++ {
+		j := i + 1
+		for j < n {
+			if A[j] < A[i] {
+				temp := A[i]
+				A[i] = A[j]
+				A[j] = temp
+			}
+			j++
 		}
-		A[j+1] = key
 	}
 }
