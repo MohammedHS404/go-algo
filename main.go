dcbb95b077636b093ba3ab14202f70b75ac5035b
mohamed.shokry <mohamed.shokry@flairstech.com>
2024-10-20 01:14:25 +0300

    recursive insetion sort

--- main.go
+++ main.go
@@ -3,20 +3,28 @@ package main
 func main() {
 	A := []int{5, 2, 4, 6, 1, 3}
 	n := len(A)
-	insertionSort(A, n)
+	insertionSort(A, 0, n)
 	for i := 0; i < n; i++ {
 		println(A[i])
 	}
 }
 
-func insertionSort(A []int, n int) {
-	for i := 1; i < n; i++ {
-		key := A[i]
-		j := i - 1
-		for j >= 0 && A[j] > key {
-			A[j+1] = A[j]
-			j--
-		}
-		A[j+1] = key
+func insertionSortSort(A []int, i int) {
+	key := A[i]
+	j := i - 1
+	for j >= 0 && A[j] > key {
+		A[j+1] = A[j]
+		j--
 	}
+	A[j+1] = key
+}
+
+func insertionSort(A []int, i int, n int) {
+	if i == n {
+		return
+	}
+
+	insertionSortSort(A, i)
+
+	insertionSort(A, i+1, n)
 }
