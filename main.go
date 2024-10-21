02cf5c964bf36a01cf2189dda37ce4a77ac5d04a
mhs404 <mohammedhs404@gmail.com>
2024-10-21 11:54:15 +0300

    matrix

--- main.go
+++ main.go
@@ -2,3 +2,13 @@ package main
 
 func main() {
 }
+
+func matrix_multiply(A [][]int, B [][]int, C [][]int, n int) {
+	for i := 0; i < n; i++ {
+		for j := 0; j < n; j++ {
+			for c := 0; c < n; c++ {
+				c[i][j]=
+			}
+		}
+	}
+}
