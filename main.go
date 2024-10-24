538f8678610945434e7f6ab04a1813e300db8bce
mhs404 <mohammedhs404@gmail.com>
2024-10-24 13:23:12 +0300

    initial solution

--- main.go
+++ main.go
@@ -1,13 +1,25 @@
 package main
 
 func main() {
+	a := [][]int{{1, 2}, {3, 4}}
+	b := [][]int{{1, 2}, {3, 4}}
+	c := [][]int{{0, 0}, {0, 0}}
+
+	matrix_multiply(a, b, c, 2)
+
+	for i := 0; i < 2; i++ {
+		for j := 0; j < 2; j++ {
+			print(c[i][j], " ")
+		}
+		println()
+	}
 }
 
 func matrix_multiply(A [][]int, B [][]int, C [][]int, n int) {
 	for i := 0; i < n; i++ {
 		for j := 0; j < n; j++ {
-			for c := 0; c < n; c++ {
-				c[i][j]=
+			for k := 0; k < n; k++ {
+				C[i][j] += A[i][k] * B[k][j]
 			}
 		}
 	}
