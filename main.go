d4c6934ad77af11053ce03b4d834cb269a9f3236
mhs404 <mohammedhs404@gmail.com>
2024-10-26 15:46:26 +0300

    initial solution

--- main.go
+++ main.go
@@ -1,4 +1,50 @@
 package main
 
+import "fmt"
+
 func main() {
+
+	head := NewNode(3)
+	head.Append(5)
+	head.Append(8)
+	head.Append(5)
+	head.Append(10)
+	head.Append(2)
+	head.Append(1)
+
+	fmt.Println(head.String())
+
+	x := 5
+
+	var bxNode *Node[int] = NewNode(-1)
+	bxHead := bxNode
+
+	var axNode *Node[int] = NewNode(-1)
+	axHead := axNode
+
+	var current = head
+
+	for current != nil {
+		if current.Data < x {
+			if bxNode.Data == -1 {
+				bxNode.Data = current.Data
+			} else {
+				bxNode.Append(current.Data)
+				bxNode = bxNode.Next
+			}
+		} else {
+			if axNode.Data == -1 {
+				axNode.Data = current.Data
+			} else {
+				axNode.Append(current.Data)
+				axNode = axNode.Next
+			}
+		}
+
+		current = current.Next
+	}
+
+	bxNode.Next = axHead
+
+	fmt.Println(bxHead.String())
 }
