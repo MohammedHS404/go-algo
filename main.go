b5c2b1417aec2e81a930de8f03754759681907b7
mhs404 <mohammedhs404@gmail.com>
2024-10-26 15:14:41 +0300

    cleaning

--- main.go
+++ main.go
@@ -1,34 +1,4 @@
 package main
 
-import "fmt"
-
 func main() {
-	head := NewNode(1)
-	head.Append(2)
-	head.Append(3)
-	head.Append(4)
-	head.Append(5)
-	head.Append(6)
-	head.Append(7)
-
-	node := head
-
-	k := 6
-
-	for i := 0; i < k; i++ {
-		node = node.Next
-	}
-
-	deleteMiddleNode(node)
-
-	fmt.Println(head.String())
-}
-
-func deleteMiddleNode(node *Node[int]) {
-	if node == nil || node.Next == nil {
-		return
-	}
-
-	node.Data = node.Next.Data
-	node.Next = node.Next.Next
 }
