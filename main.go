ea00fa4a7a40872a88268cda64f9278f2cb6a2c1
mhs404 <mohammedhs404@gmail.com>
2024-11-16 23:51:45 +0200

    permutation without identity

--- main.go
+++ main.go
@@ -1,23 +1,37 @@
 package main
 
 import (
-	"fmt"
 	"math/rand/v2"
 )
 
 func main() {
 	a := []string{"cat", "dog", "Rabbit", "Rat", "Turtle"}
-	randomPermute(a)
 
-	for _, v := range a {
-		fmt.Println(v)
+	mp := make(map[string]map[int]int)
+
+	for i := 0; i < 100000; i++ {
+		randomPermuteWithoutIdentity(a)
+		for j := 0; j < len(a); j++ {
+			mp[a[j]][j]++
+		}
 	}
+
+	for i := 0; i < len(a); i++ {
+		for j := 0; j < len(a); j++ {
+			print(mp[a[i]][j], " ")
+		}
+		println()
+	}
+
 }
 
-func randomPermute[T any](a []T) {
+func randomPermuteWithoutIdentity[T any](a []T) {
 	n := len(a)
 	for i := 0; i < n; i++ {
-		temp := rand.IntN(n)
+		temp := i + rand.IntN(n)
+		for temp == i {
+			temp = rand.IntN(n)
+		}
 		a[i], a[temp] = a[temp], a[i]
 	}
 }
