62598f1586919dbbddfb58771c282f701fbe1ab4
mhs404 <mohammedhs404@gmail.com>
2024-11-17 00:33:42 +0200

    permute all without identity

--- main.go
+++ main.go
@@ -56,3 +56,48 @@ func permuteWithoutIdentity[T any](a []T) {
 		a[i], a[temp] = a[temp], a[i]
 	}
 }
+
+func permuteAllWithoutIdentity[T any](a []T) {
+	n := len(a)
+	for i := 0; i < n; i++ {
+		tempBefore := -1
+
+		if i > 0 {
+			if i == 1 {
+				tempBefore = 0
+			} else {
+				tempBefore = rand.IntN(i)
+			}
+		}
+
+		tempAfter := -1
+
+		if i < n-1 {
+			if i == n-2 {
+				tempAfter = n - 1
+			} else {
+				tempAfter = rand.IntN(n-1-i) + i + 1
+			}
+		}
+
+		temp := -1
+
+		if tempBefore != -1 && tempAfter != -1 {
+			temp = rand.IntN(2)
+
+			if temp == 0 {
+				a[i], a[tempBefore] = a[tempBefore], a[i]
+			} else {
+				a[i], a[tempAfter] = a[tempAfter], a[i]
+			}
+		} else if tempBefore != -1 {
+			a[i], a[tempBefore] = a[tempBefore], a[i]
+		} else if tempAfter != -1 {
+			a[i], a[tempAfter] = a[tempAfter], a[i]
+		} else {
+			panic("Dont know what to do")
+		}
+
+		a[i], a[temp] = a[temp], a[i]
+	}
+}
